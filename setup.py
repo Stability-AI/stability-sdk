634cd965e494769d9a1048dfdea0c4fc34a9dc80
David Marx <david.marx84@gmail.com>
2022-08-21 20:55:37 -0700

    added setup metadata

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='stability-sdk',
     version='0.0.1',
-    ########################
+    author='stability.ai',
+    author_email='api@stability.ai',
+    url='https://beta.dreamstudio.ai/',
     install_requires=[
         'Pillow',
         'grpcio',
         'grpcio-tools',
         'python-dotenv',
     ],
-    ########################
     packages=find_packages(
         where='src',
         include=['stability_sdk*'],
-        #include=['pkg*'],
-        #exclude=['additional*'],
     ),
     package_dir = {"": "src"}
-    # directory containing all the packages (e.g.  src/mypkg, src/mypkg/subpkg1, ...)
 )
