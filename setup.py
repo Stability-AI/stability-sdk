44bfd8638278b1f8cafdaf1ed4c3a699a1c2a022
David Marx <david.marx84@gmail.com>
2022-08-21 21:14:06 -0700

    added setup metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
 
+with open('README.md','r') as f:
+    README = f.read()
+
 setup(
     name='stability-sdk',
     version='0.0.1',
     author='stability.ai',
     author_email='api@stability.ai',
+    maintainer='David Marx',
+    maintainer_email='david@stability.ai',
     url='https://beta.dreamstudio.ai/',
+    download_url='https://github.com/Stability-AI/stability-sdk/',
+    
+    description='Python SDK for interacting with stability.ai APIs',
+    long_description=README,
+
     install_requires=[
         'Pillow',
         'grpcio',
@@ -16,5 +26,28 @@ setup(
         where='src',
         include=['stability_sdk*'],
     ),
-    package_dir = {"": "src"}
+    package_dir = {"": "src"},
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'Topic :: Artistic Software',
+        'Topic :: Education',
+        'Topic :: Games/Entertainment',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+        'Topic :: Multimedia',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: 3D Rendering',
+        'Topic :: Multimedia :: Graphics :: Editors',
+        'Topic :: Multimedia :: Graphics :: Editors :: Raster-Based',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Scientific/Engineering :: Image Processing',
+    ],
+    keywords=[],
+    license='',
 )
