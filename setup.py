bca1911e718af377f18e93d9e061ff2fabf09ac7
David Marx <david.marx84@gmail.com>
2022-08-21 19:03:59 -0700

    fleshing out package metadata

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stability-sdk',
+    version='0.0.1',
+    ########################
+    install_requires=[
+        'Pillow',
+        'grpcio',
+        'grpcio-tools',
+        'python-dotenv',
+    ],
+    ########################
+    packages=find_packages(
+        where='src',
+        #include=['pkg*'],
+        #exclude=['additional*'],
+    ),
+    package_dir = {"": "src"}
+    # directory containing all the packages (e.g.  src/mypkg, src/mypkg/subpkg1, ...)
+)
