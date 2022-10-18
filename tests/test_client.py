d210409a8a378a850e54b965c759b91fa3cd5627
David Marx <david.marx84@gmail.com>
2022-10-17 22:15:24 -0700

    moved to utils tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -24,28 +24,6 @@ def test_StabilityInference_init_nokey_insecure_host():
     class_instance = client.StabilityInference(host='foo.bar.baz')
     assert True
 
-@pytest.mark.parametrize("sampler_name", client.algorithms.keys())
-def test_get_sampler_from_str_valid(sampler_name):
-    client.get_sampler_from_str(s=sampler_name)
-    assert True
-
-def test_get_sampler_from_str_invalid():
-    try:
-        client.get_sampler_from_str(s='not a real sampler')
-        assert False
-    except ValueError:
-        assert True
-
-def test_truncate_fit():
-    client.truncate_fit(
-        prefix='foo', 
-        prompt='bar', 
-        ext='.baz', 
-        ts=0,
-        idx=0, 
-        max=99)
-    assert True
-
 def test_image_to_prompt():
     im = Image.new('RGB',(1,1))
     prompt = client.image_to_prompt(im, init=False, mask=False)
