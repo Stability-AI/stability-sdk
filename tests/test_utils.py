8324c201e7e27dec0c45b227fe6d1dca53a7b2d4
David Marx <david.marx84@gmail.com>
2022-10-17 22:14:44 -0700

    Create test_utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,31 @@
+import pytest
+
+from stability_sdk.utils import (
+    algorithms,
+    get_sampler_from_str,
+    truncate_fit,
+)
+
+
+@pytest.mark.parametrize("sampler_name", algorithms.keys())
+def test_get_sampler_from_str_valid(sampler_name):
+    get_sampler_from_str(s=sampler_name)
+    assert True
+
+def test_get_sampler_from_str_invalid():
+    try:
+        get_sampler_from_str(s='not a real sampler')
+        assert False
+    except ValueError:
+        assert True
+
+def test_truncate_fit():
+    truncate_fit(
+        prefix='foo', 
+        prompt='bar', 
+        ext='.baz', 
+        ts=0,
+        idx=0, 
+        max=99)
+    assert True
+ 
