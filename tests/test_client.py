5464e731e6980e64b8a9c499520a2b69dac3744a
David Marx <david.marx84@gmail.com>
2022-10-24 23:35:54 -0700

    cleanup + notes

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,6 +6,8 @@ import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 import stability_sdk.interfaces.gooseai.generation.generation_pb2_grpc as generation_grpc
 
 import grpc
+
+# feel like we should be using this, not sure how/where
 import grpc_testing
 
 from typing import Generator
@@ -52,15 +54,11 @@ def test_image_to_prompt_init_mask():
     except ValueError:
         assert True
 
-##############################
-
-# let's get dangerous
 
 def test_server_mocking(grpc_server, grpc_addr):
-    #print(grpc_addr)
     class_instance = client.StabilityInference(host=grpc_addr[0])
-    # fuck it, let's see what happens.
     response = class_instance.generate(prompt="foo bar")
     print(response)
-    # might need this link later: https://stackoverflow.com/questions/54541338/calling-function-that-yields-from-a-pytest-fixture
+    # might need this link later:
+    # - https://stackoverflow.com/questions/54541338/calling-function-that-yields-from-a-pytest-fixture
     assert isinstance(response, Generator)
