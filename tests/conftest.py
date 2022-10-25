a026e8e0869c579b6cd44d1c39d4e6aa0f250206
David Marx <david.marx84@gmail.com>
2022-10-24 20:38:49 -0700

    added missing import

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,8 @@ import pytest
 # hopefully this add anything to the PATH we might need?
 #import stability_sdk
 
+import pathlib
+
 # uh... let's try this
 # this is necessary because of how the auto-generated code constructs its imports
 #thisPath = pathlib.Path(__file__).parent.resolve()
