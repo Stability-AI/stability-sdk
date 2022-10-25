6001d42410fbd138f4fb08baa1225dfb9e200de9
David Marx <david.marx84@gmail.com>
2022-10-24 19:53:30 -0700

    attempting to mock a local grpc server

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+# modified from https://github.com/justdoit0823/grpc-resolver/blob/master/tests/conftest.py
+
+from concurrent import futures
+
+import grpc
+import pytest
+
+#from grpcresolver import EtcdServiceResolver, EtcdServiceRegistry
+#import hello_pb2_grpc
+#import rpc
+
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+import stability_sdk.interfaces.gooseai.generation.generation_pb2_grpc as generation_grpc
+
+_TEST_GRPC_PORT = (
+    '127.0.0.1:50031', '127.0.0.1:50032', '127.0.0.1:50033', '127.0.0.1:50034')
+
+
+@pytest.fixture(scope='module')
+def grpc_addr():
+    return _TEST_GRPC_PORT
+
+  
+#class Greeter(helloworld_pb2_grpc.GreeterServicer):
+#    def SayHello(self, request, unused_context):
+#        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+
+@pytest.fixture(scope='module')
+def grpc_server(grpc_addr):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    #hello_pb2_grpc.add_HelloServicer_to_server(rpc.HelloGRpcServer(), server)
+    servicer = generation_grpc.GenerationServiceServicer()
+    generation_grpc.add_GenerationServiceServicer_to_server(servicer, server)
+    for addr in grpc_addr:
+        server.add_insecure_port(addr)
+
+    server.start()
+
+    yield server
+
+    server.stop(0)
