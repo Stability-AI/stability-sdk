cd62edec07dd1584e31aeaebb467b6ada7da18e8
David Marx <david.marx84@gmail.com>
2022-11-02 12:15:13 -0700

    added args fixture, fixed variable name

--- tests/test_animator.py
+++ tests/test_animator.py
@@ -1,14 +1,17 @@
 from stability_sdk.animation import Animator, AnimationArgs
 from types import SimpleNamespace
 
-def test_init():
+#def test_init_args():
+#    AnimationArgs()
+
+def test_init_animator(default_anim_args):
     Animator(
-        #args=SimpleNamespace(),
+        args=default_anim_args,
         #out_dir='.',
         #animation_prompts=None,
         #negative_prompt=None,
         #negative_prompt_weight=None
     )
 
-def test_init_args():
-    AnimationArgs()
+def test_save_settings(default_anim_args):
+    Animator(args=default_anim_args).save_settings()
