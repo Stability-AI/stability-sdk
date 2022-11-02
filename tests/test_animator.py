dd79a4b98b2b4dd37794983d43e07a4fd844e74b
David Marx <david.marx84@gmail.com>
2022-11-02 12:25:23 -0700

    test_get_weights

--- tests/test_animator.py
+++ tests/test_animator.py
@@ -14,4 +14,13 @@ def test_init_animator(default_anim_args):
     )
 
 def test_save_settings(default_anim_args):
-    Animator(args=default_anim_args).save_settings()
+    artist=Animator(args=default_anim_args)
+    artist.save_settings()
+
+def test_get_weights(default_anim_args):
+    artist = Animator(args=default_anim_args)
+    artist.get_animation_prompts_weights(
+        frame_idx=0,
+        key_frame_values=[0],
+        interp=False
+    )
