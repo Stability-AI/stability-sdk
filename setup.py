1f82f61ecb99961d4e2e16f25b1bf24f2c607505
David Marx <david.marx84@gmail.com>
2022-11-24 13:05:58 -0500

    bump version for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md','r') as f:
 
 setup(
     name='stability-sdk',
-    version='0.2.9',
+    version='0.2.10',
     author='Wes Brown',
     author_email='wesbrown18@gmail.com',
     maintainer='David Marx',
