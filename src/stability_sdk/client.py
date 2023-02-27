6bb4fc9322818e6149274c8023e55006c22a05c6
DESKTOP-FP8LE75\coold <cooldude242@gmail.com>
2023-02-26 20:33:45 -0600

    anima_stable_dont_raise

--- src/stability_sdk/client.py
+++ src/stability_sdk/client.py
@@ -720,8 +720,6 @@ class Api:
                 if isinstance(request, generation.Request) and request.HasField("image"):
                     request.image.seed[:] = [seed + 1 for seed in request.image.seed]
                     logger.warning(f"  adjusting seed, will retry {self._max_retries-attempt} more times")
-                else:
-                    raise ce
             except grpc.RpcError as rpc_error:
                 if attempt == self._max_retries:
                     raise rpc_error
