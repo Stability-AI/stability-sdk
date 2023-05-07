7dfbdb263a1cf10fdf8c7d8243d01f7ae4565008
Stephan Auerhahn <stephan@stability.ai>
2023-05-07 07:57:18 +0000

    update model class, functional prototype

--- src/stability_sdk_sagemaker/model.py
+++ src/stability_sdk_sagemaker/model.py
@@ -1,31 +1,77 @@
 from enum import Enum
 from sagemaker import ModelPackage, Predictor
 from stability_sdk.api import GenerationRequest, CreateRequest, CreateResponse
+from typing import Optional, Any
+import json
 
-class StabilityModel(Enum):
+
+class StabilityModelRelease(Enum):
     SDXL_BETA_V2_2_2 = "stable-diffusion-xl-beta-v2-2-2"
 
-modelArns = { SDXL_BETA_V2_2_2: "arn:aws:sagemaker:us-east-1:865070037744:model-package/stable-diffusion-xl-beta-v2-2-2"}
 
-supportedInstanceTypes 
+modelArns = {
+    StabilityModelRelease.SDXL_BETA_V2_2_2: "arn:aws:sagemaker:us-east-1:740929234339:model-package/stable-diffusion-xl-beta-v2-2-2-rc1"
+}
+
 
 class StabilityModelPackage(ModelPackage):
-    def __init__(self, role, model:StabilityModel, sagemaker_session=None):
+    endpoint_name: Optional[str] = None
+    sagemaker_session: Optional[Any] = None
+    predictor: Optional[Predictor] = None
+
+    def __init__(
+        self,
+        role,
+        model: StabilityModelRelease,
+        sagemaker_session=None,
+        existing_endpoint_name=None,
+    ):
         super().__init__(
             role=role,
             model_package_arn=modelArns[model],
-            sagemaker_session=sagemaker_session
-        )         
+            sagemaker_session=sagemaker_session,
+        )
+        self.sagemaker_session = sagemaker_session
+        if existing_endpoint_name is not None:
+            self.attach(existing_endpoint_name)
 
-    def deploy(self, initial_instance_count, instance_type='ml.g5.xlarge', endpoint_name=None, **kwargs):        
-        self.endpoint:Predictor = super().deploy(
+    def deploy(
+        self,
+        initial_instance_count,
+        instance_type="ml.g5.xlarge",
+        endpoint_name=None,
+        **kwargs
+    ):
+        self.endpoint_name = endpoint_name
+        predictor = super().deploy(
             initial_instance_count=initial_instance_count,
             instance_type=instance_type,
             endpoint_name=endpoint_name,
+            wait=True,
             **kwargs
         )
-        return self.endpoint
+        if predictor is None:
+            # This does not get set for some reason
+            predictor = Predictor(
+                endpoint_name=endpoint_name, sagemaker_session=self.sagemaker_session
+            )
+        self.predictor = predictor
+        return predictor
 
-    def predict(self, input:GenerationRequest, endpoint_name=None):
-        data = CreateRequest(input)
-        return CreateResponse(self.endpoint.predict(data=data))
+    def attach(self, endpoint_name):
+        self.endpoint_name = endpoint_name
+        self.predictor = Predictor(
+            endpoint_name=endpoint_name, sagemaker_session=self.sagemaker_session
+        )
+        return self.predictor
+
+    def predict(self, input: GenerationRequest):
+        if not self.endpoint_name or not self.predictor:
+            raise ValueError("Model not deployed")
+        data = input.json(exclude_unset=True)
+        print(data)
+        endpoint_args = {
+            "ContentType": "application/json",
+            "Accept": "application/json;png",
+        }
+        return json.loads(self.predictor.predict(data=data, initial_args=endpoint_args))
