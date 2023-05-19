39e8c4551467cd4d69f22c09e47275919f6b193a
Stephan Auerhahn <palp@palp.org>
2023-05-19 00:47:56 +0000

    Update model arn lookup for marketplace

--- src/stability_sdk_sagemaker/models.py
+++ src/stability_sdk_sagemaker/models.py
@@ -1,7 +1,11 @@
-from boto3.session import Session
 
-stability_aws_account_id = "188650660114"
-stability_aws_account_region = "us-east-1"
+marketplace_aws_account_id = '865070037744'
 
-def get_model_package_arn(model_package_name, region_name=stability_aws_account_region, account_id=stability_aws_account_id):
-    return f"arn:aws:sagemaker:{region_name}:{account_id}:model-package/{model_package_name}"
+marketplace_model_packages = {
+    'stable-diffusion-xl-beta-v2-2-2': 'stable-diffusion-xl-beta-v2-2--80252c537a5a396280cdd21b5f8b298e'
+}
+
+def get_model_package_arn(model_package_name, region_name='us-east-1', account_id=marketplace_aws_account_id):
+    if model_package_name in marketplace_model_packages:
+        return f"arn:aws:sagemaker:{region_name}:{account_id}:model-package/{marketplace_model_packages[model_package_name]}"
+    raise Exception('Model name not found')
